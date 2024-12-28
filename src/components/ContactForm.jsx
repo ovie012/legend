81ad4637fdb58b5202723acf9be8360ed92cfd17
Emonefe Ovie <ovieemonefe012@gmail.com>
2024-12-28 17:05:15 +0100

    form validation done

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,24 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    language: "",
-    techStack: "",
-    role: "",
+    name : "",
+    nameError : false,
+    language : "",
+    languageError : false,
+    techStack : "",
+    techStackError : false,
+    role : "",
+    roleError : false,
     email : "",
-    message: "",
+    emailError : false,
+    message : "",
+    messageError : false,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
+  const formRef = {
+    name : useRef(),
+    language : useRef(),
+    techStack : useRef(),
+    role : useRef(),
+    email : useRef(),
+    message : useRef(),
+  }
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prev) => ({ ...prev, [name]: value }));
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("Thanks for your message! I'll get back to you soon.");
-    setFormData({ name: "", language: "", techStack: "", role: "", email : "", message: "" });
+    // alert("Thanks for your message! I'll get back to you soon.");
+    // setFormData({ name: "", language: "", techStack: "", role: "", email : "", message: "" });
+
+    let errors = {
+      name : "",
+      nameError : false,
+      language : "",
+      languageError : false,
+      techStack : "",
+      techStackError : false,
+      role : "",
+      roleError : false,
+      email : "",
+      emailError : false,
+      message : "",
+      messageError : false,
+    }
+
+    const nameRef = formRef.name.current.value;
+    const languageRef = formRef.language.current.value;
+    const techStackRef = formRef.techStack.current.value;
+    const roleRef = formRef.role.current.value;
+    const emailRef = formRef.email.current.value;
+    const messageRef = formRef.message.current.value;
+
+    if (nameRef.trim() === '') {
+      errors.nameError = true;
+      errors.name = 'No name? No fame! Fill it out so i can know who’s awesome.'
+    }
+    if (languageRef.trim() === '') {
+      errors.languageError = true;
+      errors.language = "Are you a Pythonista, a JavaScript wizard, or ? Let me know!"
+    }
+    if (techStackRef.trim() === '') {
+      errors.techStackError = true;
+      errors.techStack = "Share your tech stack and flex your dev muscles!";
+    }
+    if (roleRef.trim() === '') {
+      errors.roleError = true;
+      errors.role = "What’s your superpower in the team? Share your role with us!";
+    }
+    if (emailRef.trim() === '') {
+      errors.emailError = true;
+      errors.email = "No spam, just vibes, enter your email so we can stay connected!";
+    }
+    if (messageRef.trim() === '') {
+      errors.messageError = true;
+      errors.message = "Not so pro tip: Take breaks often, send a message.";
+    }
+
+    setFormData((prevState) => ({
+      ...prevState,
+      ...errors,
+    }));
   };
 
   return (
@@ -27,60 +94,66 @@ const ContactForm = () => {
         <input
           type="text"
           name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
+          ref={formRef.name}
+          placeholder=""
+          style={{ borderColor : formData.nameError && "rgb(251, 80, 80)" }}
         />
         <label>Name</label>
+        {formData.nameError && <p className="error-text">{formData.name}</p>}
       </div>
       <div className="form-group">
         <input
           type="text"
           name="language"
-          value={formData.language}
-          onChange={handleChange}
-          required
+          ref={formRef.language}
+          placeholder=""
+          style={{ borderColor : formData.languageError && "rgb(251, 80, 80)" }}
         />
         <label>Favorite Programming Language</label>
+        {formData.languageError && <p className="error-text">{formData.language}</p>}
       </div>
       <div className="form-group">
         <input
           type="text"
           name="techStack"
-          value={formData.techStack}
-          onChange={handleChange}
-          required
+          ref={formRef.techStack}
+          placeholder=""
+          style={{ borderColor : formData.techStackError && "rgb(251, 80, 80)" }}
         />
         <label>Tech Stack</label>
+        {formData.techStackError && <p className="error-text">{formData.techStack}</p>}
       </div>
       <div className="form-group">
         <input
           type="text"
           name="role"
-          value={formData.role}
-          onChange={handleChange}
-          required
+          ref={formRef.role}
+          placeholder=""
+          style={{ borderColor : formData.roleError && "rgb(251, 80, 80)" }}
         />
         <label>Role</label>
+        {formData.roleError && <p className="error-text">{formData.role}</p>}
       </div>
       <div className="form-group">
         <input
           type="text"
           name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
+          ref={formRef.email}
+          placeholder=""
+          style={{ borderColor : formData.email && "rgb(251, 80, 80)" }}
         />
         <label>Email</label>
+        {formData.email && <p className="error-text">{formData.email}</p>}
       </div>
       <div className="form-group">
         <textarea
           name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
+          ref={formRef.message}
+          placeholder=""
+          style={{ borderColor : formData.message && "rgb(251, 80, 80)" }}
         ></textarea>
         <label>Message</label>
+        {formData.message && <p className="error-text">{formData.message}</p>}
       </div>
       <button type="submit" className="btn-submit">
         Send Message
